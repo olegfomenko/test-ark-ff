6f142d814765950b675bf053956693ccba7e0df0
olegfomenko <oleg.fomenko2002@gmail.com>
2025-02-04 14:21:54 +0200

    upd

--- benches/bench_field.rs
+++ benches/bench_field.rs
@@ -1,11 +1,30 @@
-use ark_ff::BigInteger;
+use ark_bls12_377::Bls12_377;
+pub use ark_bls12_377::Fr as FF_Bls12_377Fr;
+use ark_ff::{BigInteger, UniformRand};
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use rand::{thread_rng, Rng};
+use rand::distributions::Standard;
 
 fn bench_field(ct: &mut Criterion) {
     ct.bench_function("ARK-FF", |b| {
         b.iter(|| {
             f();
         })
+    });
+
+    let mut rng = thread_rng();
+
+    ct.bench_function("BLS-ARK", |b| {
+        b.iter_batched(
+            || {
+                let mut a:FF_Bls12_377Fr = rng.sample(Standard {});
+                a
+            },
+            |a: FF_Bls12_377Fr| {
+                f1(a);
+            },
+            BatchSize::SmallInput,
+        )
     });
 }
 
@@ -21,6 +40,14 @@ fn f() {
     }
 
     std::hint::black_box(a);
+    std::hint::black_box(b);
+}
+
+fn f1(a: FF_Bls12_377Fr) {
+    let mut b = a.clone();
+    for i in 0..10000000 {
+        b = (b + a) * a;
+    }
     std::hint::black_box(b);
 }
 
